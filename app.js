05895760f98d514af8ea4996c3e78cd9f8508f24
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-03-28 12:08:44 -0700

    Passport authorization added and tested via postman

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//-----------------------------Dependencies-------------
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -17,25 +18,36 @@ mongoose.connection.on('error', function(err){
     console.log('Database error' + err);
 });
 
-//-----------------------------------------------------
-
+//--------------Sub Router var setup-------------------------------
 const users = require('./routes/users'); // sub-routing variable to use below
 
+//--------------Basic express setup---------------------------------
 const app = express();  // create instance to use
 const port = 3000;
 
+//--------------Body parser + cors Middleware-------------------------------
 app.use(bodyParser.json());
 app.use(cors());
 
-//set static folder to place front-end app in 
+//--------------Passport (Authentication) Middleware-----------------------------
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+//--------------Set path to front-end code (angular)---------- 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
+//--------------------Set path to sub router---------------------
 app.use('/users', users);  // routing to users folder where another js
 //app.use('/scores', scores); etc...
 
+//-------------Default endpoint - If get is called on home - respond with message
 app.get('/', function (req, res) {
     res.send('Invalid Enpoint');
 });
+
+//-------------------------Tells express to listen to a port-------------
 
 app.listen(port, function(){
     console.log('Server started on port ' + port);
