9d5820614443a121fd0882897bd532ab5b28b907
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-03-29 14:01:48 -0700

    Login / Login work, validators improved to check for 0 string

--- angular-src/src/app/services/validate.service.ts
+++ angular-src/src/app/services/validate.service.ts
@@ -6,12 +6,15 @@ export class ValidateService {
   constructor() { }
 
   validateRegister(user){
-    if(user.name == undefined || user.email == undefined ||
-      user.username == undefined || user.password == undefined){
+    if(user.name == undefined || user.name.length == 0 ||
+      user.email == undefined || user.email.length == 0 ||
+      user.username == undefined || user.username.length == 0 ||
+      user.password == undefined || user.password.length == 0 )
+      {
         return false;
       } 
-      else{
-
+      else 
+      {
         return true;
       }
   }
@@ -19,6 +22,19 @@ export class ValidateService {
    validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+  }
+
+  validateLogin(user){
+    if(user.username == undefined || user.username.length == 0 ||
+       user.password == undefined || user.password == 0)
+    { 
+      return false;
+    } 
+    else 
+    {
+      return true;
+    }
+    
   }
 
 }
