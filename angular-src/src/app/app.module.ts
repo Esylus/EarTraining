517edef4908d1cb4980ba4a68b08a74f9b910fab
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-04-12 17:56:02 -0700

    Added 404, page refresh, contact from spheres

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -19,6 +19,7 @@ import {SendMsgService} from './services/send-msg.service';
 
 import { FlashMessagesModule} from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const appRoutes: Routes = [
   { path:'', component: HomeComponent },
@@ -28,6 +29,8 @@ const appRoutes: Routes = [
   { path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
   { path:'profile', component: ProfileComponent, canActivate:[AuthGuard]},
   { path:'contact', component: ContactComponent },
+  {path: '404', component: NotfoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
@@ -39,7 +42,8 @@ const appRoutes: Routes = [
     HomeComponent,
     DashboardComponent,
     ProfileComponent,
-    ContactComponent
+    ContactComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
